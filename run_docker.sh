0554cf47d8da96af3b1fa2937e9c85dcd0d1b1a7
sathianpong.t <sathianpong.t@deverhood.com>
2025-08-01 16:38:42 +0700

    fix run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -9,7 +9,4 @@ docker rmi $image_name || true
 
 # Build and run the Docker container
 docker build -t $image_name .
-docker run -d --name $container_name -p 80:80 \
-    -v "$(pwd)/output:/app/output" \  # Mount output directory to persist predictions
-    -v "$(pwd)/models:/app/models" \  # Mount models directory for easy model updates
-    $image_name 
+docker run -d --name $container_name -p 80:80 -v "$(pwd)/output:/app/output" -v "$(pwd)/models:/app/models" $image_name 
