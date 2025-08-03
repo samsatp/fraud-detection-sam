3f911382022a607a8560fecafa9c7de7edb47a97
sathianpong.t <sathianpong.t@deverhood.com>
2025-08-03 14:09:40 +0700

    tear down test db

--- test.sh
+++ test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+output_db='output/test.db'
+
 # This script runs the tests for the FastAPI application using pytest.
-db_url='sqlite:///output/test.db' pytest test.py -svv
+db_url=sqlite:///$output_db pytest test.py -svv
+
+# Clean up the test database after running tests
+rm $output_db
